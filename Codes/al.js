56ba279e8f5b44d0eb8b68b8ab332da3ba1b80a2
teaSummer <see_tea@outlook.com>
2025-02-28 21:26:39 +0800

    1.5.0

--- Codes/al.js
+++ Codes/al.js
@@ -1,212 +1,185 @@
 const al = {
-    load: function(l = navigator.language, mode = this.mode.HTML, callback = function() {}) {
-        function getApplies(e, applyTo) {
-            applyTo = applyTo.trim()
-            let origin = applyTo.split(';')
-            origin.forEach(function(e, i) {
-                origin[i] = e.trim()
-            })
-            if (origin[origin.length - 1] === '') origin.splice(origin.length - 1, 1)
-            if (origin.length == 1 && origin[0].indexOf(':') == -1) {
-                let rez = {}
-                rez[origin[0]] = false
-                return rez
-            } else {
-                let rez = {}
-                origin.forEach(function(e) {
-                    let detach = e.split(':')
-                    detach.forEach(function(e, i) {
-                        detach[i] = e.trim()
-                    })
-                    rez[detach[0]] = detach[1]
-                })
-                return rez
-            }
+    load(mode = this.mode.HTML, callback = () => {}, l) {
+        const getApplies = (e, applyTo) => {
+            applyTo = applyTo.trim().split(';').map(e => e.trim()).filter(e => e);
+            return applyTo.reduce((rez, e) => {
+                const [key, value] = e.split(':').map(e => e.trim());
+                rez[key] = value !== void 0 ? value : false;
+                return rez;
+            }, {});
         }
-        if (!al.lang.hasOwnProperty(l)) {
-            if (l == "default") {
-                l = al.lang.default
-            } else {
-                if (l.indexOf("-") != -1) {
-                    var c = l.split("-")[0]
-                } else var c = l
-                if (al.lang.default_country !== void 0) {
-                    if (al.lang.default_country.hasOwnProperty(c)) {
-                        l = al.lang.default_country[c]
-                    } else l = al.lang.default
-                } else l = al.lang.default
-            }
-        }
-        var text = Object.keys(al.lang[l])
+        l = this.getUserLang(l);
+        const temp = { ...al.lang[l] };
         if (al.lang.default !== void 0) {
-            var defaultText = Object.keys(al.lang[al.lang.default])
-            if (text !== defaultText) {
-                for (let i = 0; i < defaultText.length; i++) {
-                    if (text.indexOf(defaultText[i]) == -1) {
-                        al.lang[l][defaultText[i]] = al.lang[al.lang.default][defaultText[i]]
-                        text = Object.keys(al.lang[l])
+            const defaultText = Object.keys(al.lang[al.lang.default]);
+            defaultText.forEach(key => {
+                if (!temp.hasOwnProperty(key)) {
+                    temp[key] = al.lang[al.lang.default][key];
+                }
+            });
+        }
+        Object.keys(temp).forEach(key => {
+            const p = temp[key];
+            temp[key] = p.replace(/(?<!\\)((?:\\\\)*)\$\{([^}]*)}/g, (m, s, expr) => s + (new Function('"use strict";return (' + expr + ')')()))
+                .replace(/(?<!\\)((?:\\\\)*)\{([^}]*)}/g, (m, s, key) => s + temp[key.trim()])
+                .replace(/\$(?:\\\$*)(\{[^}]*})/g, (m, s, n) => s + n);
+        });
+        if (mode == al.mode.REPLACE) {
+            const replaceElements = (selector, attrKey, replacementValue) => {
+                document.querySelectorAll(selector).forEach(e => {
+                    const regex = new RegExp(attrKey, "gi");
+                    al._setAttr(e, attrKey, al._getAttr(e, attrKey).replace(regex, replacementValue));
+                });
+            }
+            document.querySelectorAll('[al-aplto]').forEach(e => {
+                const applyTo = e.getAttribute('al-aplto');
+                const applies = getApplies(e, applyTo);
+                Object.keys(applies).forEach(i => {
+                    replaceElements(`[${i}]`, applies[i], temp[applies[i]]);
+                });
+            });
+            Object.keys(temp).forEach(key => {
+                replaceElements('[al]', key, temp[key]);
+            });
+            callback();
+            return;
+        }
+        const processElements = (e, applyTo, applies) => {
+            if (applies[Object.keys(applies)[0]] != false) {
+                Object.keys(applies).forEach(i => {
+                    al._setAttr(e, i, temp[applies[i]]);
+                });
+                callback();
+                return;
+            }
+        }
+        document.querySelectorAll('[al-aplto]').forEach(e => {
+            const applyTo = e.getAttribute('al-aplto');
+            const applies = getApplies(e, applyTo);
+            processElements(e, applyTo, applies);
+        });
+        Object.keys(temp).forEach(key => {
+            document.querySelectorAll(`[al="${key}"]`).forEach(e => {
+                const applyTo = e.getAttribute('al-aplto');
+                if (applyTo) {
+                    const applies = getApplies(e, applyTo);
+                    if (applies[Object.keys(applies)[0]] == false) {
+                        al._setAttr(e, Object.keys(applies)[0], temp[key]);
                     }
-                }
-            }
-        }
-        for (var i = 0; i < text.length; i++) {
-            var p = al.lang[l][text[i]]
-            al.lang[l][text[i]] = p.replace(/(?<!\\)((?:\\\\)*)\$\{([^}]*)}/g, function(m, s, expr) {
-                return s + Function('"use strict";return (' + expr + ')').call()
-            }).replace(/(?<!\\)((?:\\\\)*)\{([^}]*)}/g, function(m, s, key) {
-                return s + al.lang[l][key.trim()]
-            }).replace(/\\((?:\\\\)*)(\{[^}]*})/g, function(m, s, n) {
-                return s + n
-            })
-        }
-        if (mode === al.mode.REPLACE) {
-            document.querySelectorAll('[al-aplto]').forEach(function(e) {
-                let applyTo = e.getAttribute('al-aplto')
-                let applies = getApplies(e, applyTo)
-                if (applies[Object.keys(applies)[0]] !== false && applies[Object.keys(applies)[0]] !== void 0) {
-                    Object.keys(applies).forEach(function(i) {
-                        let z = new RegExp(applies[i], "gi")
-                        al._setAttr(e,i, al._getAttr(e,i).replace(z, al.lang[l][applies[i]]))
-                    })
-                }
-            })
-            for (var i = 0; i < text.length; i++) {
-                document.querySelectorAll('[al]').forEach(function(e) {
-                    let z = new RegExp(text[i], "gi")
-                    let applyTo = e.getAttribute('al-aplto')
-                    if (applyTo !== null && applyTo !== void 0) {
-                        let applies = getApplies(e, applyTo)
-                        let appliesI = Object.keys(applies)[0]
-                        if (applies[appliesI] === false) {
-                            al._setAttr(e,appliesI, al._getAttr(e,appliesI).replace(z, al.lang[l][text[i]]))
-                        }
-                        al._setAttr(e,applyTo, al._getAttr(e,applyTo).replace(z, al.lang[l][text[i]]))
-                    } else if (e.tagName.toLowerCase() == "input") {
-                        e.value = e.value.replace(z, al.lang[l][text[i]])
-                    } else e.innerHTML = e.innerHTML.replace(z, al.lang[l][text[i]])
-                })
-            }
-            callback()
-            return
-        }
-        document.querySelectorAll('[al-aplto]').forEach(function(e) {
-            let applyTo = e.getAttribute('al-aplto')
-            let applies = getApplies(e, applyTo)
-            if (applies[Object.keys(applies)[0]] !== false && applies[Object.keys(applies)[0]] !== void 0) {
-                Object.keys(applies).forEach(function(i) {
-                    al._setAttr(e, i, al.lang[l][applies[i]])
-                })
-                callback()
-                return
-            }
-        })
-        for (var i = 0; i < text.length; i++) {
-            document.querySelectorAll('[al="' + text[i] + '"]').forEach(function(e) {
-                let applyTo = e.getAttribute('al-aplto')
-                if (applyTo !== null && applyTo !== void 0) {
-                    let applies = getApplies(e, applyTo)
-                    if (applies[Object.keys(applies)[0]] === false) al._setAttr(e,Object.keys(applies)[0], al.lang[l][text[i]])
                 } else if (e.tagName.toLowerCase() == "input") {
-                    e.value = al.lang[l][text[i]]
+                    e.value = temp[key];
                 } else {
                     switch (mode) {
                         case al.mode.TEXT:
-                            e.innerText = al.lang[l][text[i]]
-                            break
+                            e.innerText = temp[key];
+                            break;
                         default:
-                            e.innerHTML = al.lang[l][text[i]]
+                            e.innerHTML = temp[key];
                     }
                 }
-            })
-        }
-        callback()
+            });
+        });
+        callback();
     },
-    setLangPropPath: function(path) {
-        if (!path.endsWith("/")) path += "/"
-        this.langPropPath = path
+    setLangPropPath(path) {
+        this.langPropPath = path;
     },
-    setLangProp: function(obj, cb = function() {}, attr = {}) {
-        if (Array.isArray(obj)) {
-            if (attr.url == true && (typeof obj[0]) == "string") {
-                this._(obj, 0, function(r) {
-                    al.setLangProp(r, cb, attr)
-                }, attr.yaml)
-            } else {
-                obj.forEach(function(e) {
-                    al.lang[e.language] = e.data
-                })
-                cb(this.lang)
-            }
-        } else if ((typeof obj) == "object") {
-            if (obj.default === void 0) {
-                obj.default = this.lang.default
-            }
-            this.lang = obj
-            cb(this.lang)
+    current(obj, attr = {}) {
+        const userLang = this.getUserLang();
+        const defaultLang = this.lang.default;
+        if (obj.indexOf(userLang) == -1) {
+            this.setLangProp([defaultLang], void 0, attr);
         } else {
-            if (attr.yaml == true) {
-                this.setLangProp(jsyaml.load(obj), cb, attr)
-            } else if (attr.yaml == false) {
-                this.setLangProp(JSON.parse(obj), cb, attr)
-            } else try {
-                this.setLangProp(jsyaml.load(obj), cb, attr)
-            } catch {
-                this.setLangProp(JSON.parse(obj), cb, attr)
-            }
+            this.setLangProp(defaultLang == userLang ? [userLang] : [defaultLang, userLang], void 0, attr);
         }
     },
-    setDefault: function(def) {
-        this.lang["default"] = def
-    },
-    setDefaultCountry: function(def) {
-        this.lang["default_country"] = def
-    },
-    release: function() {
-        al.lang = {}
-        al._p = []
-    },
-    httpGet: function(url, callback = function() {}) {
-        var xhttp = new XMLHttpRequest()
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4) {
-                callback(this.responseText, this.status)
-            }
-        }
-        xhttp.open("GET", url, true)
-        xhttp.send()
-    },
-    _p: [],
-    _: function(arr, i, cb, isYaml) {
-        if (i >= arr.length) {
-            cb(this._p)
-            this._p = []
-            return
-        }
-        this.httpGet(this.langPropPath + arr[i], function(r) {
-            if (isYaml == true) {
-                al._p.push(jsyaml.load(r))
-            } else if (isYaml == false) {
-                al._p.push(JSON.parse(r))
+    setLangProp(obj, cb = () => {}, attr = {}) {
+        if (Array.isArray(obj)) {
+            if (attr.url == true && typeof obj[0] == "string") {
+                this._(obj, 0, (r) => {
+                    this.setLangProp(r, cb, attr);
+                }, attr.yaml);
             } else {
-                try {
-                    al._p.push(jsyaml.load(r))
-                } catch {
-                    al._p.push(JSON.parse(r))
+                obj.forEach(e => {
+                    this.lang[e.language] = e.data;
+                });
+                cb(this.lang);
+            }
+        } else if (typeof obj == "object") {
+            if (obj.default === void 0) {
+                obj.default = this.lang.default;
+            }
+            this.lang = obj;
+            cb(this.lang);
+        } else {
+            try {
+                this.setLangProp(jsyaml.load(obj), cb, attr);
+            } catch {
+                this.setLangProp(JSON.parse(obj), cb, attr);
+            }
+        }
+    },
+    setDefault(def) {
+        this.lang["default"] = def;
+    },
+    setDefaultCountry(def) {
+        this.lang["default_country"] = def;
+    },
+    getUserLang(l) {
+        if (!l) l = navigator.language;
+        if (!this.lang.hasOwnProperty(l)) {
+            if (l == "default") {
+                l = this.lang.default;
+            } else {
+                const c = l.indexOf("-") != -1 ? l.split("-")[0] : l;
+                if (this.lang.default_country !== void 0) {
+                    if (this.lang.default_country.hasOwnProperty(c)) {
+                        l = this.lang.default_country[c];
+                    } else {
+                        l = this.lang.default;
+                    }
+                } else {
+                    l = this.lang.default;
                 }
             }
-            al._(arr, i + 1, cb, isYaml)
-        })
+        }
+        return l;
     },
-    _setAttr: function(e, key, value) {
-        e[key] = value
-        e.setAttribute(key, value)
+    release() {
+        this.lang = {};
+        this._p = [];
     },
-    _getAttr: function(e, key) {
-        if (e[key] !== void 0) return e[key]
-        return e.getAttribute(key)
+    httpGet(url, callback = () => {}) {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = () => {
+            if (xhttp.readyState == 4) {
+                callback(xhttp.responseText, xhttp.status);
+            }
+        };
+        xhttp.open("GET", url, true);
+        xhttp.send();
+    },
+    _p: [],
+    _(arr, i = 0, cb, isYaml) {
+        if (i >= arr.length) {
+            cb(this._p);
+            this._p = [];
+            return;
+        }
+        this.httpGet(this.langPropPath.replace(/%%/g, arr[i]), (r) => {
+            this._p.push(isYaml == true ? jsyaml.load(r) : JSON.parse(r));
+            this._(arr, i + 1, cb, isYaml);
+        });
+    },
+    _setAttr(e, key, value) {
+        e[key] = value;
+        e.setAttribute(key, value);
+    },
+    _getAttr(e, key) {
+        return e[key] !== void 0 ? e[key] : e.getAttribute(key);
     },
     langPropPath: "",
-    ver: [16, "1.4.5"],
+    ver: [17, "1.5.0"],
     mode: {
         HTML: 0,
         TEXT: 1,
